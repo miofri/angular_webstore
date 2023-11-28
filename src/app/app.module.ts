5a0d7f3a2e4c428ecdf03e91fac7a91894c5cbce
selvi <slvkesuma@gmail.com>
2023-11-28 22:56:56 +0200

    updating angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,33 +6,37 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { ProductsComponent } from './products/products.component';
-import { ProductsEffects } from './store/effects/products.effects';
-import { productsReducer } from './store/reducers/products.reducer';
+// import { ProductsEffects } from './store/effects/products.effects';
+// import { productsReducer } from './store/reducers/products.reducer';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { CartComponent } from './cart/cart.component';
 import { ProfileComponent } from './profile/profile.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { ManageProductsComponent } from './manage-products/manage-products.component';
 import { ManageUsersComponent } from './manage-users/manage-users.component';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductsComponent,
-    ProductDetailComponent,
-    CartComponent,
-    ProfileComponent,
-    AdminDashboardComponent,
-    ManageProductsComponent,
-    ManageUsersComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    StoreModule.forRoot({ products: productsReducer }, {}),
-    EffectsModule.forRoot([ProductsEffects])
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		ProductsComponent,
+		ProductDetailComponent,
+		CartComponent,
+		ProfileComponent,
+		AdminDashboardComponent,
+		ManageProductsComponent,
+		ManageUsersComponent,
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		// StoreModule.forRoot({ products: productsReducer }, {}),
+		// EffectsModule.forRoot([ProductsEffects]),
+		StoreModule.forRoot({}, {}),
+		EffectsModule.forRoot([]),
+		StoreRouterConnectingModule.forRoot(),
+	],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
